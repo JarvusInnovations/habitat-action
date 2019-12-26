9778dcf41023a9941df74d62f7640aaba103f9d2
Chris Alfano <chris@jarv.us>
2019-12-25 22:23:24 -0500

    style: skip framing output

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ async function run() {
 
     // verify installation (and initialize license)
     try {
-        console.log(`Installed: ${await execOutput('hab --version')}`);
+        await exec('hab --version');
     } catch (err) {
         core.setFailed(`Failed to verify hab installation: ${err.message}`);
         return;
